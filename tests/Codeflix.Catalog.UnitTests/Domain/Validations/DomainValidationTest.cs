32301c04e3a4e16f94a6f4a7ae3123827e391888
Edson Santos <dev@escode.net>
2024-08-14 21:22:17 -0300

    Add MinLength validation method and tests
    Introduce a new MinLength validation method in DomainValidation.cs to ensure string fields meet minimum length requirements. Also, add corresponding unit tests in DomainValidationTest.cs to verify the new validation logic works as expected.

--- tests/Codeflix.Catalog.UnitTests/Domain/Validations/DomainValidationTest.cs
+++ tests/Codeflix.Catalog.UnitTests/Domain/Validations/DomainValidationTest.cs
@@ -9,47 +9,47 @@ namespace Codeflix.Catalog.UnitTests.Domain.Validations;
 public class DomainValidationTest
 {
     private Faker Faker { get; set; } = new Faker();
-    
+
     [Fact(DisplayName = nameof(NotNullOk))]
     public void NotNullOk()
     {
         //Arrange
         var value = Faker.Commerce.ProductName();
-        
+
         //Act
-        var action = () => DomainValidation.NotNull(value, "Value");
-        
+        var action = () => DomainValidation.NotNull(value, "FieldName");
+
         //Assert
         action.Should().NotThrow();
     }
-    
+
     [Fact(DisplayName = nameof(NotNullThrowWhenNull))]
     public void NotNullThrowWhenNull()
     {
         //Arrange
         string? value = null;
-        
+
         //Act
         var action = () => DomainValidation.NotNull(value, "FieldName");
-        
+
         //Assert
         action.Should().Throw<EntityValidationException>()
             .WithMessage("FieldName should not be null");
     }
-    
+
     [Fact(DisplayName = nameof(NotNullOrEmptyOk))]
     public void NotNullOrEmptyOk()
     {
         //Arrange
         var value = Faker.Commerce.ProductName();
-        
+
         //Act
-        var action = () => DomainValidation.NotNullOrEmpty(value, "Value");
-        
+        var action = () => DomainValidation.NotNullOrEmpty(value, "FieldName");
+
         //Assert
         action.Should().NotThrow();
     }
-    
+
     [Theory(DisplayName = nameof(NotNullOrEmptyThrowWhenEmpty))]
     [InlineData("")]
     [InlineData("   ")]
@@ -58,10 +58,48 @@ public class DomainValidationTest
     {
         //Arrange
         var action = () => DomainValidation.NotNullOrEmpty(target, "FieldName");
-        
+
         //Act
         //Assert
         action.Should().Throw<EntityValidationException>()
             .WithMessage("FieldName should not be null or empty");
+    }
+
+    [Fact(DisplayName = nameof(MinLengthOk))]
+    public void MinLengthOk()
+    {
+        //Arrange
+        var value = Faker.Commerce.ProductName();
+
+        //Act
+        var action = () => DomainValidation.MinLength(value, minLength: 3, "FieldName");
+
+        //Assert
+        action.Should().NotThrow();
+    }
+
+    [Theory(DisplayName = nameof(MinLengthThrowWhenLess))]
+    [MemberData(nameof(GetValuesSmallerThanTheMin), parameters: 10)]
+    public void MinLengthThrowWhenLess(string target, int minLength)
+    {
+        //Arrange
+        var action = () => DomainValidation.MinLength(target, minLength, "FieldName");
+
+        //Act
+        //Assert
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage($"FieldName should be at leats {minLength} characters long");
+    }
+
+    public static IEnumerable<object[]> GetValuesSmallerThanTheMin(int numberOfTests = 6)
+    {
+        var faker = new Faker();
+
+        for (var i = 0; i < numberOfTests; i++)
+        {
+            var target = faker.Commerce.ProductName();
+            var minLength = target.Length + new Random().Next(1, 20);
+            yield return [target, minLength];
+        }
     }
 }
